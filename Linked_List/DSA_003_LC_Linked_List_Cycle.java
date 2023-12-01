b284e48a9fcd88b8c29f651c82f24d9cc6579458
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2023-12-01 11:19:11 +0530

    Update DSA_003_LC_Linked_List_Cycle.java

--- Linked_List/DSA_003_LC_Linked_List_Cycle.java
+++ Linked_List/DSA_003_LC_Linked_List_Cycle.java
@@ -1,5 +1,17 @@
 package Linked_List;
 
 public class DSA_003_LC_Linked_List_Cycle {
-    
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+
+        }
+        return false;
+    }
 }
