2b4ecd9e5a409258e00c35029d2a198af65569a4
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2023-12-02 17:22:35 +0530

    Create DSA_012_Sort_List.java

---
+++ Linked_List/DSA_012_Sort_List.java
@@ -0,0 +1,46 @@
+package Linked_List;
+
+import java.util.List;
+
+public class DSA_012_Sort_List {
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return mergeTwoLists(left, right);
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode node = new ListNode();
+        ListNode head = node;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                head.next = list1;
+                list1 = list1.next;
+            } else {
+                head.next = list2;
+                list2 = list2.next;
+            }
+            head = head.next;
+        }
+        head.next = (list1 != null) ? list1 : list2;
+        return node.next;
+    }
+
+    public ListNode getMid(ListNode head) {
+        ListNode midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        ListNode mid = midPrev.next;
+        return mid;
+    }
+
+
+}
