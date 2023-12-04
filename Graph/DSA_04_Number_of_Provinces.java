4e973742f80c0a5b9740702e374e37805c2e3959
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2023-12-04 13:40:09 +0530

    Update DSA_04_Number_of_Provinces.java

--- Graph/DSA_04_Number_of_Provinces.java
+++ Graph/DSA_04_Number_of_Provinces.java
@@ -4,26 +4,27 @@ import java.util.ArrayList;
 
 public class DSA_04_Number_of_Provinces {
     private static void dfs(int node,
-                            ArrayList<ArrayList<Integer>> adjLs ,
+                            ArrayList<ArrayList<Integer>> adjLs,
                             int vis[]) {
         vis[node] = 1;
-        for(Integer it: adjLs.get(node)) {
-            if(vis[it] == 0) {
+        for (Integer it : adjLs.get(node)) {
+            if (vis[it] == 0) {
                 dfs(it, adjLs, vis);
             }
         }
     }
+
     static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
         ArrayList<ArrayList<Integer>> adjLs = new ArrayList<ArrayList<Integer>>();
-        for(int i = 0;i<V;i++) {
+        for (int i = 0; i < V; i++) {
             adjLs.add(new ArrayList<Integer>());
         }
 
         // to change adjacency matrix to list
-        for(int i = 0;i<V;i++) {
-            for(int j = 0;j<V;j++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
                 // self nodes are not considered
-                if(adj.get(i).get(j) == 1 && i != j) {
+                if (adj.get(i).get(j) == 1 && i != j) {
                     adjLs.get(i).add(j);
                     adjLs.get(j).add(i);
                 }
@@ -31,19 +32,19 @@ public class DSA_04_Number_of_Provinces {
         }
         int vis[] = new int[V];
         int cnt = 0;
-        for(int i = 0;i<V;i++) {
-            if(vis[i] == 0) {
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
                 cnt++;
                 dfs(i, adjLs, vis);
             }
         }
         return cnt;
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
 
         // adjacency matrix
-        ArrayList<ArrayList<Integer> > adj = new ArrayList<ArrayList<Integer> >();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
 
         adj.add(new ArrayList<Integer>());
         adj.get(0).add(0, 1);
@@ -59,6 +60,6 @@ public class DSA_04_Number_of_Provinces {
         adj.get(2).add(2, 1);
 
         DSA_04_Number_of_Provinces ob = new DSA_04_Number_of_Provinces();
-        System.out.println(ob.numProvinces(adj,3));
+        System.out.println(ob.numProvinces(adj, 3));
     }
 }
