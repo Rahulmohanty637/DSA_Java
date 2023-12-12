207fce3067fa03e679eac4fc0ef405d230ba1d58
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2023-12-12 14:59:20 +0530

    Create DAS_015_Rotate_List.java

---
+++ Linked_List/DAS_015_Rotate_List.java
@@ -0,0 +1,27 @@
+package Linked_List;
+
+public class DAS_015_Rotate_List {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        ListNode temp = head;
+        int n = 1;
+        while (temp.next != null) {
+            n++;
+            temp = temp.next;
+        }
+        temp.next = head;
+        k = k % n;
+        n = n - k;
+
+        ListNode newLast = head;
+        for (int i = 0; i < n - 1; i++) {
+            newLast = newLast.next;
+        }
+        head = newLast.next;
+        newLast.next = null;
+
+        return head;
+    }
+}
