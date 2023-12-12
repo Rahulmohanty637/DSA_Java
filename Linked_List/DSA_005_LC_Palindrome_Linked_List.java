74141202bcdc6b5a1e9374a78b0105d053e24e9a
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2023-12-12 15:18:51 +0530

    Update DSA_005_LC_Palindrome_Linked_List.java

--- Linked_List/DSA_005_LC_Palindrome_Linked_List.java
+++ Linked_List/DSA_005_LC_Palindrome_Linked_List.java
@@ -1,32 +1,49 @@
 package Linked_List;
 
 public class DSA_005_LC_Palindrome_Linked_List {
-    public boolean isPalindrome(ListNode head) {
-        ListNode slow = head, fast = head, prev, temp;
+    public ListNode findMiddle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
         while (fast != null && fast.next != null) {
-            slow = slow.next;
             fast = fast.next.next;
-        }
-        prev = slow;
-        slow = slow.next;
-        prev.next = null;
-        while (slow != null) {
-            temp = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = temp;
-        }
-        fast = head;
-        slow = prev;
-        while (slow != null) {
-            if (fast.val != slow.val) return false;
-            fast = fast.next;
             slow = slow.next;
         }
-        return true;
 
+        return slow;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode pres = head;
+        ListNode next = head.next;
+
+        while (pres != null) {
+            pres.next = prev;
+            prev = pres;
+            pres = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
     }
 
 
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = findMiddle(head);
+        ListNode headSecond = reverseList(mid);
+        ListNode headFirst = head;
+        while (headFirst != null && headSecond != null) {
+            if (headFirst.val != headSecond.val) {
+                return false;
+            }
+            headFirst = headFirst.next;
+            headSecond = headSecond.next;
+        }
+        return true;
+    }
 
 }
