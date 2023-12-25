2c976b0ec2f47c99d44d391f4f9d7bcc9a39d8cf
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2023-12-26 00:00:20 +0530

    Create DSA_LC_Power_of_Power_of_Three.java

---
+++ Problems/DSA_LC_Power_of_Power_of_Three.java
@@ -0,0 +1,13 @@
+package Problems;
+
+public class DSA_LC_Power_of_Power_of_Three{
+        public boolean isPowerOfThree(int n) {
+            if (n == 0) {
+                return false;
+            }
+            while (n % 3 == 0) {
+                n /= 3;
+            }
+            return n == 1;
+        }
+}
