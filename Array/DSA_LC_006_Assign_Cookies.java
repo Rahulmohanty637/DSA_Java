4af3b441cba21038755ea482b9f4eb77cade8fa2
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-01-01 21:39:09 +0530

    Create DSA_LC_006_Assign_Cookies.java

---
+++ Array/DSA_LC_006_Assign_Cookies.java
@@ -0,0 +1,14 @@
+package Array;
+
+import java.util.Arrays;
+
+public class DSA_LC_006_Assign_Cookies {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for(int j=0;i<g.length && j<s.length;j++)
+            if(g[i]<=s[j]) i++;
+        return i;
+    }
+}
