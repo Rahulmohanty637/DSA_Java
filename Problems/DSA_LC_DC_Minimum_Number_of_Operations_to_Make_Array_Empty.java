a49f6c0d99c165f1e2fbff7407387b4cf5b25835
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-01-04 23:07:17 +0530

    updated

--- Problems/DSA_LC_DC_Minimum_Number_of_Operations_to_Make_Array_Empty.java
+++ Problems/DSA_LC_DC_Minimum_Number_of_Operations_to_Make_Array_Empty.java
@@ -1,40 +1,31 @@
 package Problems;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DSA_LC_DC_Minimum_Number_of_Operations_to_Make_Array_Empty {
     public static int minOperations(int[] nums) {
-        int op = 0;
-        int freq = 0;
-        int ans = 0;
-        int[] arr = null;
-        int j = 0;
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                freq++;
-            } else {
-                arr[j] = freq;
-                j++;
-                freq = 0;
-            }
-        }
 
-        for (int i = 0; i < arr.length; i++) {
-            if ((arr[i] - 1) % 3 == 0) {
-                ans += 2;
-                ans += (arr[i] - 4) / 3;
-            } else if ((arr[i] - 2) % 3 == 0) {
-                ans += 1;
-                ans += (arr[i] - 2) / 3;
-            }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i:nums)
+            map.put(i, map.getOrDefault(i,0)+1);
+        int ans = 0;
+        for(int v:map.values()){
+            if(v==1)
+                return -1;
+            if(v%3 == 0)
+                ans += v/3;
+            else if((v-2)%3 == 0)
+                ans = ans + 1 + (v-2)/3;
+            else if((v-4)%3 == 0)
+                ans = ans + 2 + (v-4)/3;
         }
         return ans;
-
     }
 
     public static void main(String[] args) {
-        int[] nums = {16, 19, 31, 8, 10};
+        int[] nums = {2, 3, 3, 2, 2, 4, 2, 3, 4, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4};
         System.out.println(minOperations(nums));
     }
 }
