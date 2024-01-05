41876b38e28a26550e71867ec0811e04fa8e9b57
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-01-05 23:41:30 +0530

    Create DSA_LC_018_Container_With_Most_Water.java

---
+++ Array/DSA_LC_018_Container_With_Most_Water.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class DSA_LC_018_Container_With_Most_Water {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+}
