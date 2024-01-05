144b1124312ff0055ff3ea41c9a23f25a5ff3151
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-01-05 13:56:01 +0530

    LC

---
+++ Problems/DSA_LC_DC_Longest_Increasing_Subsequence.java
@@ -0,0 +1,26 @@
+package Problems;
+
+import java.util.Arrays;
+
+public class DSA_LC_DC_Longest_Increasing_Subsequence {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int maxLength = Arrays.stream(dp).max().orElse(0);
+        return maxLength;
+    }
+}
