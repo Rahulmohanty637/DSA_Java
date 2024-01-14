23bab2a1b1b6483e151681e813e1cea41cbe577b
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-01-14 21:11:08 +0530

    Create CustomStack.java

---
+++ Stack/CustomStack.java
@@ -0,0 +1,32 @@
+package Stack;
+
+public class CustomStack {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+    int ptr = -1;
+
+    public CustomStack() {
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomStack(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean pushData(int item) {
+        if (isFull()) {
+            return false;
+        }
+        ptr++;
+        data[ptr] = item;
+        return true;
+    }
+
+    private boolean isFull() {
+        return ptr == data.length - 1;
+    }
+
+    private boolean isEmpty() {
+        return ptr == -1;
+    }
+}
