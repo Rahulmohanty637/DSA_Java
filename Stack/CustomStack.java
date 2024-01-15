0cd951e66d6f92cb24ea9de5cdc4972b926dc634
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-01-15 12:01:05 +0530

    dynamic stack

--- Stack/CustomStack.java
+++ Stack/CustomStack.java
@@ -36,11 +36,11 @@ public class CustomStack {
         return data[ptr];
     }
 
-    private boolean isFull() {
+    public boolean isFull() {
         return ptr == data.length - 1;
     }
 
-    private boolean isEmpty() {
+    public boolean isEmpty() {
         return ptr == -1;
     }
 }
