5bc6ccdc6ec1ace9ed989a6f31bf04e14e0266a8
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-01-15 13:10:17 +0530

    Create CustomQueue.java

---
+++ Queue/CustomQueue.java
@@ -0,0 +1,59 @@
+package Queue;
+
+public class CustomQueue {
+    private int[] data;
+    private static final int DEFAULT_SIZE = 10;
+    int end = -1;
+
+    public CustomQueue() {
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomQueue(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean insertData(int item) {
+        if (isFull()) {
+            return false;
+        }
+        data[end++] = item;
+        return true;
+    }
+
+    public int removedData() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+        int removed = data[0];
+        for (int i = 1; i < end; i++) {
+            data[i - 1] = data[i];
+        }
+        return removed;
+    }
+
+    public int front() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is Empty");
+        }
+        return data[0];
+    }
+
+    public void displayData() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is Empty");
+        }
+        for (int i = 0; i < end; i++) {
+            System.out.println(data[i] + " ");
+        }
+        System.out.println("END");
+    }
+
+    public boolean isFull() {
+        return end == data.length;
+    }
+
+    public boolean isEmpty() {
+        return end == 0;
+    }
+}
