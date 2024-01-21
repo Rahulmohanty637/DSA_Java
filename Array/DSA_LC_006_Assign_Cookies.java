11383d30223d55adbf965e504b155cd60edcd95e
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-01-21 09:09:48 +0530

    d

--- Array/DSA_LC_006_Assign_Cookies.java
+++ Array/DSA_LC_006_Assign_Cookies.java
@@ -7,8 +7,8 @@ public class DSA_LC_006_Assign_Cookies {
         Arrays.sort(g);
         Arrays.sort(s);
         int i = 0;
-        for(int j=0;i<g.length && j<s.length;j++)
-            if(g[i]<=s[j]) i++;
+        for (int j = 0; i < g.length && j < s.length; j++)
+            if (g[i] <= s[j]) i++;
         return i;
     }
 }
