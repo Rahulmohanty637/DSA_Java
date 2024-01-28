d1b9687077cc38faa4647a139a8b96d6d3b5d1d4
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-01-28 23:38:49 +0530

    Create DSA_LC_005_Add_Binary.java

---
+++ String/DSA_LC_005_Add_Binary.java
@@ -0,0 +1,20 @@
+package String;
+
+public class DSA_LC_005_Add_Binary {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0)
+                carry += a.charAt(i--) - '0';
+            if (j >= 0)
+                carry += b.charAt(j--) - '0';
+            sb.append(carry % 2);
+            carry /= 2;
+        }
+        return sb.reverse().toString();
+    }
+}
