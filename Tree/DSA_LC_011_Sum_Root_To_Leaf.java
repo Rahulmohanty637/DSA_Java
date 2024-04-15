9500400c1b199e9e548dc020f68e3ac726737e99
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-04-15 23:36:25 +0530

    Create DSA_LC_011_Sum_Root_To_Leaf.java

---
+++ Tree/DSA_LC_011_Sum_Root_To_Leaf.java
@@ -0,0 +1,39 @@
+package Tree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+public class DSA_LC_011_Sum_Root_To_Leaf {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int pathSum) {
+        if (node == null)
+            return 0;
+
+        pathSum = pathSum * 10 + node.val;
+
+        if (node.left == null && node.right == null)
+            return pathSum;
+
+        return dfs(node.left, pathSum) + dfs(node.right, pathSum);
+    }
+}
